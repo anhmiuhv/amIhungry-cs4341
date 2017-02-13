4e8c7a07cbcf4d588eeaf98adcba809dcf9c4f8e
Duong Nguyen <nguyendu@proctornet.com>
2017-02-13 16:36:44 -0500

    Update main.py: a dumb way to read input like in the assignment. (For easier use)

---
+++ main.py
@@ -0,0 +1,64 @@
+# Author:
+#     Khoi Doan
+#     Linh Hoang
+#     Sam Huang
+#     Duong Nguyen
+
+
+#!/usr/bin/env python3
+import argparse
+
+#parsing option from users
+parser = argparse.ArgumentParser(description='Please give mr.weather a needed information')
+parser.add_argument('query', help='the query node')
+parser.add_argument('iteration', type = int, help='the number of iteration run')
+parser.add_argument('observed', nargs=argparse.REMAINDER,help='the observed node')
+#parser.add_argument('observed', metavar='N', nargs='+',help='the observed node')
+args = parser.parse_args()
+
+
+queryList = []
+queryCondition = []
+query = ''
+condition = ''
+take = 0
+for i in args.query:
+    if i == '=':
+        take = 1
+    else:
+        if take:
+            condition +=i
+        else:
+            query += i
+        
+queryList.append(query)
+queryCondition.append(condition)
+print(queryList)
+print(queryCondition)
+observedList = []
+observedCondition = []
+query = ''
+condition = ''
+take = 0
+for i in range(0,len(args.observed)):
+    for j in args.observed[i]:
+        if j == '=':
+            take = 1
+        else:
+            if take:
+                condition +=j
+            else:
+                query += j
+    observedList.append(query)
+    observedCondition.append(condition)
+    query = ''
+    condition = ''
+    take = 0
+    
+print(observedList)
+print(observedCondition)
+# class Node:
+#     def __init__(self, initial, condition):
+#         for i in initial:
+#             if i == '=':
+#                 print('noob')
