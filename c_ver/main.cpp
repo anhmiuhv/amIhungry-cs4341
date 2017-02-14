bb26420b7be7de0ea68bea6aba897c54a1870437
lthoang@wpi.edu <anhmiuhv@yahoo.com.vn>
2017-02-14 18:58:17 -0500

    c version

---
+++ c_ver/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+int main(int argc, char** argv){
+  if (argc < 4) {
+    cout << "too few argument" << endl;
+  }
+
+  for (int i = 0; i < argc; i++){
+    
+  }
+  
+}
